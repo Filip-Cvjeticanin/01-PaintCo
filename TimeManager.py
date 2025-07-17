58ccb63fcf4b6a908948bb6790ce2ed2d732b956
Filip-Cvjeticanin <filip.cvjeticanin@fer.unizg.hr>
2025-07-17 14:15:04 +0200

    Class reorganization + TimeManager module

---
+++ TimeManager.py
@@ -0,0 +1,25 @@
+from PyQt5.QtCore import QTimer
+
+
+class TimeManager:
+    def __init__(self):
+        self.timeElapsed = 0
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.tick)
+
+    def start(self):
+        print("Starting application...")
+        self.timer.start(10)
+
+    def stop(self):
+        self.timer.stop()
+
+    def getTime(self):
+        return self.timeElapsed
+
+    def tick(self):
+        self.timeElapsed += 10
+        #if self.timeElapsed % 1000 == 0:
+        #    sec = int(self.timeElapsed / 1000)
+        #    msec = int(self.timeElapsed - sec * 1000)
+        #    print(f"[{sec:0>4}:{msec:0>3}]: second passed!")
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
+from TimeManager import TimeManager
 
 import sys
 
@@ -11,33 +12,31 @@ class MyMainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-def salute():
-    print("Hello from button!")
+        self.setMinimumSize(800, 600)
+        self.setMaximumSize(1200,900)
+        self.setWindowTitle("Paint CO")
+        self.show()
+
+        self.CentralWidget = QWidget()
+        self.setCentralWidget(self.CentralWidget)
+        self.CentralLayout = QVBoxLayout()
+        self.CentralWidget.setLayout(self.CentralLayout)
+
+        self.saluteButton = QPushButton("Salute!")
+        self.saluteButton.clicked.connect(self.salute)
+        self.CentralLayout.addWidget(self.saluteButton)
+
+    def salute(self):
+        time = timeManager.getTime()
+        sec = int(time / 1000)
+        msec = int(time - sec * 1000)
+        print(f"[{sec:0>4}:{msec:0>3}]: I salute you!")
+
 
 app = QApplication(sys.argv)
-
-
-
-
-
-
 window = MyMainWindow()
-window.setWindowTitle("Paint CO")
-window.show()
-window.resize(800, 600)
-window.show()
-
-widget = QWidget()
-window.setCentralWidget(widget)
-
-layout = QVBoxLayout()
-button = QPushButton("Salute!")
-
-layout.addWidget(button)
-widget.setLayout(layout)
-
-button.clicked.connect(salute)
-
+timeManager = TimeManager()
+timeManager.start()
 
 
 
